c939217b1e1c3f771944596b1357a30da4cf15dc
KovalevSergei <ckv51@mail.ru>
2022-06-13 09:12:33 +0300

    rki

---
+++ src/repositiries/videos-repository.ts
@@ -0,0 +1,19 @@
+const videos = []
+
+export const videosRepository = {
+    getVideos() {
+
+    },
+    getVideoById(id: number) {
+
+    },
+    deleteVideoById(id: number) {
+
+    },
+    updateVideoById(id: number, title: string) {
+
+    },
+    createVideo(ititle: string) {
+
+    }
+}
